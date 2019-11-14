3caf7745dd9ba0b3b73d7aa87ff850edcdbca56f
Matthew Jones <matts3371@gmail.com>
2019-11-14 16:44:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,21 @@
 # Add  code here!
 def prime_test(number)
-  if number = 1
-  range = (1..number).to_a 
-  is_prime = true 
-  range.each do |numOne|
-    range.each do |numTwo|
-      if numOne * numTwo == number 
-        is_prime = false 
+  is_prime = false 
+  if number == 1 
+    is_prime = false 
+  elsif number == 2 
+    is_prime = true 
+  else 
+    range = (1..number).to_a 
+    is_prime = true 
+    range.each do |numOne|
+      range.each do |numTwo|
+        if numOne * numTwo == number 
+          is_prime = false 
+        end 
       end 
-    end 
-  end
+    end
+  end 
   is_prime 
 end 
   
